4553a2367473b2ce59f9f5566dd03efe6a1cf767
Charles Langlois <charles.langlois.dev@gmail.com>
2024-12-15 17:21:27 +0100

    Fix/error/action/form/buttons.tsx

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
@@ -26,10 +28,22 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  // const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const handleDelete = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const result = await deleteInvoice(id);
+      if (result.message) {
+        console.log(result.message);
+      }
+    } catch (error) {
+      console.error('Failed to delete invoice:', error);
+    }
+  };
 
   return (
-    <form action={deleteInvoiceWithId}>
+    <form onSubmit={handleDelete}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-4" />
